ed3e6aa2861e3c7a7fea515dc9c0f57b0b64b2a5
Arjav <arjavpatni@hotmail.com>
2020-04-25 17:49:33 +0530

    StatesService created to parse JSON using Volley.

--- app/src/main/java/com/example/covid19_india/View/MainActivity.kt
+++ app/src/main/java/com/example/covid19_india/View/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.covid19_india.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.covid19_india.R
+import com.example.covid19_india.Services.StatesService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var statesAdapter : StatesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        statesAdapter = StatesAdapter(this, StatesService.states)
+        states_view.adapter = statesAdapter
+        val layoutManager = LinearLayoutManager(this)
+        states_view.layoutManager = layoutManager
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
